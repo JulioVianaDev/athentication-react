b0057a292f3a6dba9fd90f27dbd9baed403b5c97
juliovianadev <juliovianadev@gmail.com>
2023-03-23 15:48:43 -0300

    criando a action de login

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,7 +1,41 @@
 import AuthForm from '../components/AuthForm';
-
+import {json,redirect} from 'react-router-dom'
 function AuthenticationPage() {
   return <AuthForm />;
 }
 
 export default AuthenticationPage;
+
+export async function action({request}){
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get('mode') || 'login';
+  if(mode !== 'login' && mode !== 'signup'){
+    throw json({message: 'modo insuportavel pelo meu app'},{status: 422});
+  }
+
+  const data = await request.formData();
+  const authData = {
+    email: data.get('email'),
+    password: data.get('password')
+  }
+
+  const response = await fetch('http://localhost:8080/'+mode,{
+    method: 'POST',
+    headers: {
+      'Content-Type':'application/json'
+    },
+    body: JSON.stringify(authData)
+  });
+
+  if(response.status === 422 || response.status === 401){
+    return response
+  }
+
+  if(!response.ok){
+    throw json({message: "não foi possivel autenticar o usuario"},{status:500})
+  }
+
+  // agora peguei o token
+  return redirect('/');
+
+}
