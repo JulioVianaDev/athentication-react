c87490ebbc6c524674d56c31736ce4e1554180fb
juliovianadev <juliovianadev@gmail.com>
2023-03-24 00:25:38 -0300

    finalizando o modulo

--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -1,6 +1,22 @@
 import { redirect } from "react-router";
+
+export function getTokenDuration(){
+  const storagedExpirationDate = localStorage.getItem('expiration');
+  const expirationDate = new Date(storagedExpirationDate);
+  const now = new Date();
+  const duration = expirationDate.getTime() - now.getTime();
+  return duration
+}
+
 export function getAuthToken(){
   const token = localStorage.getItem('token');
+  const tokenDuration = getTokenDuration();
+  if (!token){
+    return
+  }
+  if (tokenDuration<0){
+    return 'EXPIRED'
+  }
   return token;
 }
 
